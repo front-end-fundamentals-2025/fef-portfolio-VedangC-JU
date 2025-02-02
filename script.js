f9dfff4b79b0d057d4dbc5555e870745f4b403ff
VedangC-JU <chve24wm@student.ju.se>
2025-02-02 17:15:44 +0100

    Javascript addition
    Creating a script.js file and linking it to every HTML file.

---
+++ script.js
@@ -0,0 +1,4 @@
+window.addEventListener("load", function() {
+    document.getElementById("loading-screen").style.display = "none"; // Hide loading screen
+    document.body.style.opacity = "1"; // Show page content
+});
